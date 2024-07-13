7e66b9ca980e35ca760c787dfbbd72e39a66d37c
Santiago A. Bermudez <69102034+Santiago13225@users.noreply.github.com>
2024-07-13 12:05:44 -0700

    Add files via upload

--- CSC 138/client.py
+++ CSC 138/client.py
@@ -1,8 +1,19 @@
+#UDP Client
+from socket import *
+serverName = 'hostname' //here, 127.0.0.1
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence:')
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+clientSocket.close()
+
 #TCP Client
 '''
 from socket import *
 serverName = 'servername' //here, 127.0.0.1
-serverPort = 12061
+serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 sentence = raw_input('Input lowercase sentence:')
