bb2f6db5d44ef8f15675a3a0a7d80a213318418b
Santiago A. Bermudez <69102034+Santiago13225@users.noreply.github.com>
2024-07-13 12:08:51 -0700

    Add files via upload

---
+++ CSC 190/ComputerVision.py
@@ -0,0 +1,38 @@
+
+print('Settings')
+print('1. Toggle Online/Offline Computation')
+print('2. Toggle Multiple/Single Object Classification for Live Capture')
+print('3. FPS for Live Capture')
+print('Option:')
+
+print('1')
+print('Do you want to handle processing offline or online?')
+print('(*1 for offline, 2 for online):')
+
+print('1')
+print('Done! Processing is now online.')
+
+print('1')
+print('Done! Processing is now offline.')
+
+
+print('2')
+print('Do you want to classify a single object or multiple objects for Live Capture?')
+print('(*1 for single, 2 for multiple):')
+
+
+print('2')
+print('Done! Single object capture is now activated!')
+
+
+print('2')
+print('Done! Multiple object capture is now activated!')
+
+
+print('3')
+print('Please enter the framerate you want to use for live image processing.')
+print('Hint: You can enter a value between 1 and 60:')
+
+
+print('3')
+print('Done! Framerate now set to 50!')
