f59f9bdfae1c319bad14babb072e352818697aca
Santiago13225 <santi.2001bermudez@gmail.com>
2025-01-02 17:37:58 -0800

    Added CSC 130 Book

--- CSC 138/UDPClient.py
+++ CSC 138/UDPClient.py
@@ -1,15 +1,16 @@
 #UDP Client
-from socket import *
-serverName = 'hostname' //here, 127.0.0.1
-serverPort = 12000
-clientSocket = socket(AF_INET, 
- SOCK_DGRAM)
-message = raw_input('Input lowercase sentence:')
+from socket import * #Includes Python’s socket library!
+serverName = '127.0.0.1'
+serverPort = 12061
+clientSocket = socket(AF_INET, SOCK_DGRAM) #Creates a UDP socket for the server.
+#^Creates a datagram with server IP and port = x;
+message = raw_input('Input lowercase sentence:') #Gets user input from the keyboard.
 clientSocket.sendto(message,(serverName, serverPort))
-modifiedMessage, serverAddress = 
- clientSocket.recvfrom(2048)
-print modifiedMessage
-clientSocket.close()
+#^Attaches the server name and port to the message and then sends it into the socket.
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+#^Simply reads the reply characters from the socket into string.
+print modifiedMessage #Prints out the received string.
+clientSocket.close()  #Closes the socket.
 
 #TCP Client
 '''
--- CSC 138/UDPServer.py
+++ CSC 138/UDPServer.py
@@ -1,13 +1,13 @@
 #UDP Server
 from socket import *
-serverPort = 12000
+serverPort = 12061
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print 'The server is ready to receive'
 while 1:
- message, clientAddress = serverSocket.recvfrom(2048)
- modifiedMessage = message.upper()
- serverSocket.sendto(modifiedMessage, clientAddress)
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.upper()
+    serverSocket.sendto(modifiedMessage, clientAddress)
  
 #TCP Server
 '''
